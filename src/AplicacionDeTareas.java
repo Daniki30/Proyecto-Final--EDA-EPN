9c7ec914d6b587a16ab396fb9029543908f5c219
Danna Feria <89428652+Daniki30@users.noreply.github.com>
2022-03-03 01:58:55 -0500

    feat: add and complete menu

--- src/AplicacionDeTareas.java
+++ src/AplicacionDeTareas.java
@@ -5,55 +5,47 @@ public class AplicacionDeTareas {
     static int select = -1;
     public static void main(String[] args) {
         ListaDeTareas lista = new ListaDeTareas();
-        lista.añadirTarea("hacer deberes ","dx");
-        lista.añadirTarea("1","dfadaf");
-        lista.añadirTarea("1","2");
-        lista.eliminarTarea();
-        lista.editarTarea(0,"1","2121");
-        System.out.println(lista);
-        while(select != 0){
+        lista.añadirTarea("Deber 1", "dx");
 
-                String lectura = JOptionPane.showInputDialog(null,"************************\nElige opción:\n1.- Ver Lista de Tareas" +
-                        "\n2.-Añadir Tarea\n" +
-                        "3.- Eliminar Tarea\n" +
-                        "4.- Editar Tarea \n" +
-                        "0.- Salir\n************************");
-                //Recoger una variable por consola
-                select = Integer.parseInt(lectura);
+        while (select != 0) {
 
-                //Switch case en Java
-                switch(select){
-                    case 1:
+            String lectura = JOptionPane.showInputDialog(null,
+                    "************************\nElige opción:\n" +
+                            "1.- Ver Lista de Tareas\n" +
+                            "2.- Añadir Tarea\n" +
+                            "3.- Eliminar Última Tarea\n" +
+                            "4.- Editar Tarea \n" +
+                            "0.- Salir\n************************");
+            //Recoger una variable por consola
+            select = Integer.parseInt(lectura);
 
-                        JOptionPane.showMessageDialog(null,lista);
+            //Switch case en Java
+            switch (select) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, lista
+                    );
+                    break;
+                case 2:
 
-                        break;
-                    case 2:
-
-                        JOptionPane.showMessageDialog(null,"");
-
-                        break;
-                    case 3:
-
-                        JOptionPane.showMessageDialog(null,"" );
-                        break;
-                    case 4:
-
-                        JOptionPane.showMessageDialog(null,"");
-                        break;
-                    case 0:
-                        JOptionPane.showMessageDialog(null,"");
-                        break;
-                    default:
-                        JOptionPane.showMessageDialog(null,"");break;
-                }
-
-
-
-
+                    JOptionPane.showMessageDialog(null, lista.añadirTarea(
+                            JOptionPane.showInputDialog(null, "Nombre de la tarea: "),
+                            JOptionPane.showInputDialog(null, "Descripción: ")));
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, lista.eliminarTarea());
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null, lista.editarTarea(
+                            Integer.parseInt(JOptionPane.showInputDialog(null,"Número de tarea a editar: ")),
+                            JOptionPane.showInputDialog(null, "Nombre de la tarea: "),
+                            JOptionPane.showInputDialog(null, "Descripción: ")));
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "VUELVA PRONTO <3");
+                    break;
+            }
         }
     }
-
 }
 
 
